1736a3423fdad13ddaeedd84452889f258554631
Alexej Wolff <awolff@draug.me>
2016-04-25 21:46:41 +0200

    Some bug fixes are done. Now should works fine!

--- bot.js
+++ bot.js
@@ -2,17 +2,18 @@
 
 var TelegramBot = require('node-telegram-bot-api');
 
-/* Get you Telegram Bot Token @BotFater*/
-var TOKEN = 'TOKEN';
-/* Create your own API Token on https://steamapi.loveandcoffee.io */
-var STEAM_API_TOKEN = 'STEAM_API_TOKEN'
-
-var request = require("request")
-
-
+/* Get you Telegram Bot Token @BotFater */
+var TOKEN = ''; 
 var bot = new TelegramBot(TOKEN, {polling: {timeout: 1, interval: 100}});
 
+/* STEAM API URL */
 var STEAM_PRICE_API = 'https://steamapi.loveandcoffee.io/api/prices/730/{0}?key={1}';
+/* Create your own API Token on https://steamapi.loveandcoffee.io */
+var STEAM_API_TOKEN = ''; 
+
+//Load the request module
+var request = require('request')
+
 
 var opts = {
   reply_markup: JSON.stringify(
@@ -20,7 +21,6 @@ var opts = {
       force_reply: true
     }
   )};
-
 
 // Replacement like in Python
 if (!String.prototype.format) {
@@ -42,28 +42,24 @@ bot.on('message', function(msg){
   console.log(msg);
   if(msg.text == "/start") 
     bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
-
 });
-
 
 /* Get name - return lowest price */
 bot.on('message', function(msg){
   var chatId = msg.chat.id;
   var name;
   if(msg.text == name) 
-    var url = STEAM_PRICE_API.format(msg.text, STEAM_API_TOKEN)
     request({
-        url: url,
+        // URL - FORMATED STEAM_PRICE_API + msg.text + STEAM_API_TOKEN
+        url: STEAM_PRICE_API.format(msg.text, STEAM_API_TOKEN),
         json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          bot.sendMessage(chatId, body['market_hash_name'] + " - " + body['prices']['lowest'] + "$");
-        }
+    }, function(error, response, data){
+        if (!error && response.statusCode === 200) 
+          bot.sendMessage(chatId, data['market_hash_name'] + " - " + data['prices']['lowest'] + "$");
     })
 });
 
-
-/* Dont Die BITCH!*/
+/* Dont Die BITCH! */
 process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err);
 });
