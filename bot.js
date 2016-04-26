bf902272a211b05c82ce2be4fd17e500102672b7
Alexej Wolff <awolff@draug.me>
2016-04-26 14:47:42 +0200

    Added command to get CSGO Server Status

--- bot.js
+++ bot.js
@@ -13,16 +13,17 @@ var TelegramBot = require('node-telegram-bot-api');
 
 /* Get you Telegram Bot Token @BotFater */
 var TOKEN = '';
-
-
 var bot = new TelegramBot(TOKEN, {polling: {timeout: 1, interval: 100}});
-
 
 /* Create your own API Token on https://steamapi.loveandcoffee.io */
 var STEAM_API_TOKEN = ''; 
+/* Create your own Steam API Token on https://steamcommunity.com/dev/apikey */
+var STEAM_OF_API_TOKEN = '';
 
 /* Steam Api URL */
 var STEAM_PRICE_API = 'https://steamapi.loveandcoffee.io/api/prices/730/{0}?key={1}';
+/* Steam CSGO Server Status API */
+var STEAM_SERVER_STATUS_API = 'https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key={0}';
 
 //Load the request module
 var request = require('request')
@@ -55,6 +56,25 @@ bot.on('message', function(msg){
   if(msg.text == "/start") 
     bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
 
+});
+
+/* Get server status for CSGO */
+bot.on('message', function(msg){
+  var chatId = msg.chat.id;
+  if(msg.text == "/status") {
+    var url = '';
+    url = STEAM_SERVER_STATUS_API.format(STEAM_OF_API_TOKEN);
+    request({
+      url: url,
+      json: true
+    }, function(error, response, data){
+        if(!error && response.statusCode == 200) {
+          bot.sendMessage(chatId, "Server status: " + data['result']['matchmaking']['scheduler'] + ". Online Servers: " + data['result']['matchmaking']['online_servers'] + ". Online Players: " + data['result']['matchmaking']['online_players'] + ". Players Searching: " + data['result']['matchmaking']['searching_players'] + ".");
+        } else {
+          bot.sendMessage(chatId, "Sorry, I cant fetch data right now, try latter! :(")
+        }
+    })
+  } 
 });
 
 /* Get name - return lowest price */
