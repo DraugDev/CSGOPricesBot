13671d0ea0c5ab50157d485b59e514a746aa7ab7
Alexej Wolff <awolff@draug.me>
2016-04-29 03:16:18 +0200

    Added some new stuff. Redesigner code.

--- bot.js
+++ bot.js
@@ -16,14 +16,19 @@ var TOKEN = '';
 var bot = new TelegramBot(TOKEN, {polling: {timeout: 1, interval: 100}});
 
 /* Create your own API Token on https://steamapi.loveandcoffee.io */
-var STEAM_API_TOKEN = ''; 
+var STEAM_API_TOKEN = '';
+var BACKUP_API_TOKEN = '';
+
 /* Create your own Steam API Token on https://steamcommunity.com/dev/apikey */
 var STEAM_OF_API_TOKEN = '';
 
-/* Steam Api URL */
-var STEAM_PRICE_API = 'https://steamapi.loveandcoffee.io/api/prices/730/{0}?key={1}';
-/* Steam CSGO Server Status API */
+/* Steam Price Api URL */
+var STEAM_PRICE_API = 'https://steamapi.loveandcoffee.io/api/v1/prices/730/{0}?apikey={1}';
+var BACKUP_PRICE_API = 'http://csgo.steamlytics.xyz/api/v1/prices/{0}?key={1}';
+
+/* Steam API URL's */
 var STEAM_SERVER_STATUS_API = 'https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key={0}';
+var STEAM_USER_ID = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={0}&vanityurl={1}';
 
 //Load the request module
 var request = require('request')
@@ -39,7 +44,7 @@ var opts = {
 if (!String.prototype.format) {
   String.prototype.format = function() {
     var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
+    return this.replace(/{(\d+)}/g, function(match, number) {
       return typeof args[number] != 'undefined'
         ? args[number]
         : match
@@ -51,16 +56,19 @@ if (!String.prototype.format) {
 /* WELCOME MESSAGE */
 bot.on('message', function(msg){
   var chatId = msg.chat.id;
-  var name;
   console.log(msg);
-  if(msg.text == "/start") 
-    bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
-
+  if(msg.text == "/start") {
+    // bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
+    bot.sendMessage(chatId, "Hello. Im CSGO Prices DevBot. I will help you to get some info about this game. Use /status to get servr status. Use /price to get Skin price. Or /help");
+  }
+  if(msg.text == "/help") {
+    // bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
+    bot.sendMessage(chatId, "/start /status /price /id");
+  }
 });
 
 /* Get server status for CSGO */
 bot.on('message', function(msg){
-  var chatId = msg.chat.id;
   if(msg.text == "/status") {
     var url = '';
     url = STEAM_SERVER_STATUS_API.format(STEAM_OF_API_TOKEN);
@@ -69,29 +77,68 @@ bot.on('message', function(msg){
       json: true
     }, function(error, response, data){
         if(!error && response.statusCode == 200) {
-          bot.sendMessage(chatId, "Server status: " + data['result']['matchmaking']['scheduler'] + ". Online Servers: " + data['result']['matchmaking']['online_servers'] + ". Online Players: " + data['result']['matchmaking']['online_players'] + ". Players Searching: " + data['result']['matchmaking']['searching_players'] + ".");
+          bot.sendMessage(msg.chat.id, "Server status: " + data['result']['matchmaking']['scheduler'] + ". Online Servers: " + data['result']['matchmaking']['online_servers'] + ". Online Players: " + data['result']['matchmaking']['online_players'] + ". Players Searching: " + data['result']['matchmaking']['searching_players'] + ".");
         } else {
-          bot.sendMessage(chatId, "Sorry, I cant fetch data right now, try latter! :(")
+          bot.sendMessage(msg.chat.id, "Sorry, I cant fetch data right now, try latter! :(")
         }
     })
-  } 
+  }
 });
 
-/* Get name - return lowest price */
+
+var opts = {
+  reply_markup: JSON.stringify(
+    {
+      force_reply: true
+    }
+  )};
+
+/* Get name - return lowest price*/
 bot.on('message', function(msg){
-  var chatId = msg.chat.id;
-  var name;
-  if(msg.text == name) 
-    var url = '';
-    url = STEAM_PRICE_API.format(msg.text, STEAM_API_TOKEN);
-    request({
-        url: url,
-        json: true
-    }, function(error, response, data){
-        if (!error && response.statusCode === 200) 
-          bot.sendMessage(chatId, data['market_hash_name'] + " - " + data['prices']['last'] + "$");
-    })
+  if(msg.text == '/price') {
+    bot.sendMessage(msg.chat.id, 'What Skin you are looking for?', opts)
+     .then(function (sended) {
+        bot.onReplyToMessage(msg.chat.id, sended.message_id, function (message) {
+          var url = '';
+          url = STEAM_PRICE_API.format(message.text, STEAM_API_TOKEN);
+          /*console.log(url);*/
+          request({
+              url: url,
+              json: true
+          }, function(error, response, data){
+              if (!error && response.statusCode === 200) {
+                bot.sendMessage(msg.chat.id, data['data']['market_hash_name'] + " - " + data['data']['prices']['lowest'] + "$");
+              }
+          })
+        });
+     });
+  }
 });
+
+/* Get stem User ID*/
+bot.on('message', function(msg){
+  if(msg.text == '/id') {
+    bot.sendMessage(msg.chat.id, 'What is your custom ID? (https://steamcommunity.com/id/YOUR_ID)', opts)
+     .then(function (sended) {
+        bot.onReplyToMessage(msg.chat.id, sended.message_id, function (message) {
+          var url = '';
+          url = STEAM_USER_ID.format(STEAM_OF_API_TOKEN, message.text);
+          // console.log(url);
+          request({
+              url: url,
+              json: true
+          }, function(error, response, data){
+              if (!error && response.statusCode === 200) {
+                bot.sendMessage(msg.chat.id, "Your ID: " + data['response']['steamid']);
+              }
+          })
+        });
+     });
+  }
+});
+
+
+
 
 /* Dont Die BITCH! */
 process.on('uncaughtException', function (err) {
